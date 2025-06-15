305e1b8ee6893b3240f9867fc8abad4d5ad52bc3
datagore <104622881+datagore@users.noreply.github.com>
2025-06-15 10:33:12 +0300

    Add basic lock and thread creation

--- philo/main.c
+++ philo/main.c
@@ -6,40 +6,69 @@
 /*   By: abostrom <abostrom@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/13 23:32:05 by abostrom          #+#    #+#             */
-/*   Updated: 2025/06/13 23:42:19 by abostrom         ###   ########.fr       */
+/*   Updated: 2025/06/15 10:32:53 by abostrom         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "philo.h"
 
-static int	parse_argument(const char *s)
+static int	parse_argument(const char *str)
 {
 	int	value;
 
 	value = 0;
-	while (*s == ' ' || ('\t' <= *s && *s <= '\r'))
-		s++;
-	while ('0' <= *s && *s <= '9')
-		value = value * 10 + *s++ - '0';
-	while (*s == ' ' || ('\t' <= *s && *s <= '\r'))
-		s++;
-	if (*s != '\0')
+	while (*str == ' ' || ('\t' <= *str && *str <= '\r'))
+		str++;
+	while ('0' <= *str && *str <= '9')
+		value = value * 10 + *str++ - '0';
+	while (*str == ' ' || ('\t' <= *str && *str <= '\r'))
+		str++;
+	if (*str != '\0' || value <= 0)
 	{
-		printf("Error: invalid argument\n");
+		printf("error: invalid argument\n");
 		exit(EXIT_FAILURE);
 	}
 	return (value);
 }
 
+static void	*philo_thread(void *arg)
+{
+	printf("Hi from philo thread\n");
+	return (arg);
+}
+
+static void	philo_start(t_philo *philo)
+{
+	int	i;
+
+	i = 0;
+	while (i < philo->count)
+		pthread_mutex_init(&philo->mutexes[i++], NULL);
+	i = 0;
+	while (i < philo->count)
+	{
+		if (pthread_create(&philo->threads[i++], NULL, philo_thread, NULL))
+		{
+			printf("error: pthread_create failed\n");
+			return ;
+		}
+	}
+	i = 0;
+	while (i < philo->count)
+		pthread_join(philo->threads[i++], NULL);
+}
+
 int	main(int argc, char **argv)
 {
-	t_philo philo;
+	t_philo	philo;
 
+	memset(&philo, 0, sizeof(t_philo));
 	if (argc != 5 && argc != 6)
-		return printf("usage: %s <count> <die> <eat> [sleep]\n", argv[0]);
+		return (printf("usage: %s <count> <die> <eat> [sleep]\n", argv[0]));
 	philo.count = parse_argument(argv[1]);
 	philo.starve_time = parse_argument(argv[2]);
 	philo.eat_time = parse_argument(argv[3]);
@@ -47,4 +76,12 @@ int	main(int argc, char **argv)
 	philo.target_meals = 0;
 	if (argc == 6)
 		philo.target_meals = parse_argument(argv[5]);
+	philo.threads = malloc(philo.count * sizeof(pthread_t *));
+	philo.mutexes = malloc(philo.count * sizeof(pthread_mutex_t));
+	if (philo.threads != NULL && philo.mutexes != NULL)
+		philo_start(&philo);
+	else
+		printf("error: can't allocate memory\n");
+	free(philo.threads);
+	free(philo.mutexes);
 }
--- philo/philo.h
+++ philo/philo.h
@@ -6,20 +6,44 @@
 /*   By: abostrom <abostrom@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/13 23:36:06 by abostrom          #+#    #+#             */
-/*   Updated: 2025/06/13 23:41:36 by abostrom         ###   ########.fr       */
+/*   Updated: 2025/06/15 10:33:03 by abostrom         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
+# include <pthread.h>
+
+# define MAX_EVENTS 64
+
+typedef enum e_event_type
+{
+	EVENT_TOOK_FORK,
+	EVENT_EATING,
+	EVENT_SLEEPING,
+	EVENT_THINKING,
+	EVENT_DIED
+}	t_event_type;
+
+typedef struct s_event
+{
+	t_event_type	type;
+	int				index;
+}	t_event;
+
 typedef struct s_philo
 {
-	int	count;
-	int	starve_time;
-	int	eat_time;
-	int	sleep_time;
-	int	target_meals;
+	int				count;
+	int				starve_time;
+	int				eat_time;
+	int				sleep_time;
+	int				target_meals;
+	_Atomic size_t	events_head;
+	_Atomic size_t	events_tail;
+	t_event			events[MAX_EVENTS];
+	pthread_t		*threads;
+	pthread_mutex_t	*mutexes;
 }	t_philo;
 
 #endif
