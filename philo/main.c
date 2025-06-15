56f60cdbf7e38359d44899cf3ea636cb5585a101
datagore <104622881+datagore@users.noreply.github.com>
2025-06-15 23:05:24 +0300

    Implement most states (no starving yet)

--- philo/main.c
+++ philo/main.c
@@ -6,15 +6,42 @@
 /*   By: abostrom <abostrom@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/13 23:32:05 by abostrom          #+#    #+#             */
-/*   Updated: 2025/06/15 10:32:53 by abostrom         ###   ########.fr       */
+/*   Updated: 2025/06/15 23:05:11 by abostrom         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <limits.h>
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/time.h>
+#include <unistd.h>
 
 #include "philo.h"
+
+static long	get_time(void)
+{
+	struct timeval	timeval;
+
+	gettimeofday(&timeval, NULL);
+	return (timeval.tv_sec * 1000000 + timeval.tv_usec);
+}
+
+static void	sleep_for(long usecs)
+{
+	const long	start_time = get_time();
+	const long	target_time = start_time + usecs;
+	long		difference;
+
+	difference = usecs;
+	while (difference > 0)
+	{
+		usleep(difference);
+		difference = target_time - get_time();
+	}
+}
 
 static int	parse_argument(const char *str)
 {
@@ -35,23 +62,63 @@ static int	parse_argument(const char *str)
 	return (value);
 }
 
-static void	*philo_thread(void *arg)
+static void	print_event(t_philo *philo, t_event_type type, int index)
 {
-	printf("Hi from philo thread\n");
-	return (arg);
+	static const char *const	event_string[] = {
+		"took a fork",
+		"is eating",
+		"is sleeping",
+		"is thinking",
+		"died",
+	};
+	long						timestamp;
+
+	timestamp = (get_time() - philo->start_time + 500) / 1000;
+	if (pthread_mutex_lock(&philo->print_mutex) == 0)
+	{
+		printf("%ld %d %s\n", timestamp, index, event_string[type]);
+		pthread_mutex_unlock(&philo->print_mutex);
+	}
 }
 
-static void	philo_start(t_philo *philo)
+static void	*philo_thread(void *arg)
 {
-	int	i;
+	t_philo *const	philo = (t_philo*) arg;
+	const int		index = philo->created++;
+	const int		left_fork = (index != philo->count - 1) * index;
+	const int		right_fork = (index != philo->count - 1) + index;
+	int				meals_eaten;
 
+	meals_eaten = 0;
+	while (meals_eaten++ < philo->max_meals)
+	{
+		pthread_mutex_lock(&philo->mutexes[left_fork]);
+		print_event(philo, EVENT_TOOK_FORK, index);
+		pthread_mutex_lock(&philo->mutexes[right_fork]);
+		print_event(philo, EVENT_EATING, index);
+		sleep_for(philo->eat_time);
+		print_event(philo, EVENT_SLEEPING, index);
+		pthread_mutex_unlock(&philo->mutexes[left_fork]);
+		pthread_mutex_unlock(&philo->mutexes[right_fork]);
+		sleep_for(philo->sleep_time);
+		print_event(philo, EVENT_THINKING, index);
+	}
+	return (NULL);
+}
+
+static void	philo_main(t_philo *philo)
+{
+	int		i;
+
+	philo->start_time = get_time();
+	pthread_mutex_init(&philo->print_mutex, NULL);
 	i = 0;
 	while (i < philo->count)
 		pthread_mutex_init(&philo->mutexes[i++], NULL);
 	i = 0;
 	while (i < philo->count)
 	{
-		if (pthread_create(&philo->threads[i++], NULL, philo_thread, NULL))
+		if (pthread_create(&philo->threads[i++], NULL, philo_thread, philo))
 		{
 			printf("error: pthread_create failed\n");
 			return ;
@@ -70,16 +137,16 @@ int	main(int argc, char **argv)
 	if (argc != 5 && argc != 6)
 		return (printf("usage: %s <count> <die> <eat> [sleep]\n", argv[0]));
 	philo.count = parse_argument(argv[1]);
-	philo.starve_time = parse_argument(argv[2]);
-	philo.eat_time = parse_argument(argv[3]);
-	philo.sleep_time = parse_argument(argv[4]);
-	philo.target_meals = 0;
+	philo.starve_time = 1000 * parse_argument(argv[2]);
+	philo.eat_time = 1000 * parse_argument(argv[3]);
+	philo.sleep_time = 1000 * parse_argument(argv[4]);
+	philo.max_meals = INT_MAX;
 	if (argc == 6)
-		philo.target_meals = parse_argument(argv[5]);
+		philo.max_meals = parse_argument(argv[5]);
 	philo.threads = malloc(philo.count * sizeof(pthread_t *));
 	philo.mutexes = malloc(philo.count * sizeof(pthread_mutex_t));
 	if (philo.threads != NULL && philo.mutexes != NULL)
-		philo_start(&philo);
+		philo_main(&philo);
 	else
 		printf("error: can't allocate memory\n");
 	free(philo.threads);
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: abostrom <abostrom@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/13 23:36:06 by abostrom          #+#    #+#             */
-/*   Updated: 2025/06/15 10:33:03 by abostrom         ###   ########.fr       */
+/*   Updated: 2025/06/15 23:01:57 by abostrom         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,35 +15,33 @@
 
 # include <pthread.h>
 
-# define MAX_EVENTS 64
+# define MAX_EVENTS 256
 
-typedef enum e_event_type
+typedef enum e_event_type	t_event_type;
+typedef struct s_event		t_event;
+typedef struct s_philo		t_philo;
+
+enum e_event_type
 {
 	EVENT_TOOK_FORK,
 	EVENT_EATING,
 	EVENT_SLEEPING,
 	EVENT_THINKING,
 	EVENT_DIED
-}	t_event_type;
+};
 
-typedef struct s_event
-{
-	t_event_type	type;
-	int				index;
-}	t_event;
-
-typedef struct s_philo
+struct s_philo
 {
 	int				count;
+	_Atomic int		created;
 	int				starve_time;
 	int				eat_time;
 	int				sleep_time;
-	int				target_meals;
-	_Atomic size_t	events_head;
-	_Atomic size_t	events_tail;
-	t_event			events[MAX_EVENTS];
+	int				max_meals;
 	pthread_t		*threads;
 	pthread_mutex_t	*mutexes;
-}	t_philo;
+	pthread_mutex_t	print_mutex;
+	long			start_time;
+};
 
 #endif
