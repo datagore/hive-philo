7a32f6e4f898556016ed1c3cb9c219fa6f10143f
datagore <104622881+datagore@users.noreply.github.com>
2025-06-25 13:36:57 +0300

    Try using a lock-free queue again

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: abostrom <abostrom@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/13 23:32:05 by abostrom          #+#    #+#             */
-/*   Updated: 2025/06/25 12:21:48 by abostrom         ###   ########.fr       */
+/*   Updated: 2025/06/25 13:30:07 by abostrom         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,44 @@
 
 #include "philo.h"
 
-static void	wait_for(int64_t duration)
+#define MAX_EVENTS 256
+static _Atomic uint64_t events[MAX_EVENTS];
+static _Atomic size_t events_head;
+static _Atomic size_t events_tail;
+static int64_t start_time;
+
+static void send_event(int64_t timestamp, t_state state, int index)
 {
-	const int64_t	target_time = current_time() + duration;
+	size_t head = events_head++;
+	size_t tail = events_tail;
+	if (head - tail >= MAX_EVENTS)
+		printf("error: event queue full\n");
+	while (events[head % MAX_EVENTS] != 0)
+		;
+	events[head % MAX_EVENTS] = ((timestamp - start_time) / 1000 << 32) | (index << 8) | state;
+}
+
+static bool receive_event(int *timestamp, t_state *state, int *index)
+{
+	size_t head = events_head;
+	size_t tail = events_tail;
+	if (head <= tail)
+		return (false);
+	uint64_t event = 0;
+	while (event == 0)
+		event = events[tail % MAX_EVENTS];
+	*timestamp = event >> 32;
+	*index = (event >> 8) & 0xffffff;
+	*state = event & 0xff;
+	events[events_tail++ % MAX_EVENTS] = 0;
+	return (true);
+}
+
+static void	wait_until(int64_t target_time)
+{
 	int64_t			difference;
 
-	difference = duration;
+	difference = target_time - current_time();
 	while (difference > 0)
 	{
 		usleep(difference);
@@ -31,16 +63,22 @@ static void	wait_for(int64_t duration)
 	}
 }
 
+static void	wait_for(int64_t duration)
+{
+	wait_until(current_time() + duration);
+}
+
 static void	*diner_thread(void *arg)
 {
 	t_diner *const	diner = (t_diner*) arg;
 
+	wait_until(start_time);
 	while (!diner->stop)
 	{
-		diner->state++;
+		send_event(current_time(), ++diner->state % STATE_MAX, diner->index);
 		wait_for(diner->predelay * (diner->meal_count == 0) + 500);
 		pthread_mutex_lock(diner->fork1);
-		diner->state++;
+		send_event(current_time(), ++diner->state % STATE_MAX, diner->index);
 		if (diner->fork1 == diner->fork2)
 		{
 			wait_for(diner->time_to_die);
@@ -48,14 +86,15 @@ static void	*diner_thread(void *arg)
 			break ;
 		}
 		pthread_mutex_lock(diner->fork2);
-		diner->state += 2;
+		send_event(current_time(), ++diner->state % STATE_MAX, diner->index);
+		send_event(current_time(), ++diner->state % STATE_MAX, diner->index);
 		diner->meal_time = current_time();
 		wait_for(diner->time_to_eat);
 		pthread_mutex_unlock(diner->fork1);
 		pthread_mutex_unlock(diner->fork2);
 		if (++diner->meal_count == diner->meal_limit)
 			break ;
-		diner->state++;
+		send_event(current_time(), ++diner->state % STATE_MAX, diner->index);
 		wait_for(diner->time_to_sleep);
 	}
 	return (NULL);
@@ -66,6 +105,7 @@ static int	philo_begin(t_philo *p, int arguments[5])
 	int	i;
 
 	i = 0;
+	start_time = current_time() + 10000;
 	p->count = arguments[0];
 	while (i < p->count)
 		pthread_mutex_init(&p->mutexes[i++], NULL);
@@ -76,15 +116,17 @@ static int	philo_begin(t_philo *p, int arguments[5])
 		p->diners[i].time_to_eat = 1000LL * arguments[2];
 		p->diners[i].time_to_sleep = 1000LL * arguments[3];
 		p->diners[i].meal_limit = arguments[4];
-		p->diners[i].meal_time = current_time();
+		p->diners[i].meal_time = start_time;
 		p->diners[i].fork1 = &p->mutexes[i * (i != p->count - 1)];
 		p->diners[i].fork2 = &p->mutexes[i + (i != p->count - 1)];
 		p->diners[i].predelay = (i % 2 == 0) * p->diners[i].time_to_eat;
+		p->diners[i].index = i + 1;
 		if (pthread_create(&p->threads[i], NULL, diner_thread, &p->diners[i]))
 			return (printf("error: pthread_create failed\n"));
 		p->started++;
 		i++;
 	}
+	wait_until(start_time);
 	return (0);
 }
 
@@ -93,7 +135,6 @@ static void	philo_loop(t_philo *p)
 	int		i;
 	int		finished;
 	int64_t	now;
-	int64_t	state;
 
 	finished = 0;
 	while (!p->stop && p->started == p->count && finished < p->count)
@@ -101,18 +142,20 @@ static void	philo_loop(t_philo *p)
 		i = 0;
 		finished = 0;
 		now = current_time();
+		int timestamp, index;
+		t_state state;
+		while (receive_event(&timestamp, &state, &index))
+			print_state(p, timestamp, state, index);
 		while (i < p->count)
 		{
 			state = p->diners[i].state;
 			if (p->diners[i].meal_count == p->diners[i].meal_limit)
 				finished++;
 			else if (p->diners[i].meal_time + p->diners[i].time_to_die <= now)
-				print_state(p, STATE_DIED, i);
-			while (p->states[i] < state)
-				print_state(p, ++p->states[i] % STATE_MAX, i);
+				print_state(p, (current_time() - start_time) / 1000, STATE_DIED, i);
 			i++;
 		}
-		usleep(500);
+		usleep(100);
 	}
 }
 
--- philo/util.c
+++ philo/util.c
@@ -6,7 +6,7 @@
 /*   By: abostrom <abostrom@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/24 08:18:21 by abostrom          #+#    #+#             */
-/*   Updated: 2025/06/25 11:49:13 by abostrom         ###   ########.fr       */
+/*   Updated: 2025/06/25 13:12:46 by abostrom         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,10 +35,8 @@ void	*malloc_zeroed(size_t size)
 	return (data);
 }
 
-void	print_state(t_philo *philo, t_state state, int index)
+void	print_state(t_philo *philo, int timestamp, t_state state, int index)
 {
-	static int64_t		start_time;
-	int					timestamp;
 	static char *const	state_names[] = {
 		"is sleeping",
 		"is thinking",
@@ -48,9 +46,6 @@ void	print_state(t_philo *philo, t_state state, int index)
 		"died",
 	};
 
-	if (!start_time)
-		start_time = current_time();
-	timestamp = (current_time() - start_time + 500) / 1000;
 	if (!philo->stop)
 		printf("%d %d %s\n", timestamp, index + 1, state_names[state]);
 	if (state == STATE_DIED)
